b488180a560c846b1fe9c98ba9718c21260504fe
Wilke000 <carterwilke@gmail.com>
2024-10-08 18:47:30 -0400

    Create wheelServo and functions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeFunctions.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode;
+
+import static java.lang.Thread.sleep;
+
+import android.util.Pair;
+
+import com.qualcomm.robotcore.hardware.*;
+import com.qualcomm.robotcore.hardware.configuration.typecontainers.ServoConfigurationType;
+
+public class IntakeFunctions {
+    private CRServo wheelServo;
+    private int toggleState = 0; // 0: power 0, 1: power -0.5, 2: power 0, 3: power 0.5
+
+    public IntakeFunctions(HardwareMap hardwareMap) {
+        wheelServo = hardwareMap.get(CRServo.class, "wheelServo");
+    }
+
+
+    public void initilize() throws InterruptedException {
+        // Add later
+    }
+
+    public void intakeWheelServo(boolean toggleWheelServo, float holdLT, float holdRT) {
+        if (toggleWheelServo) {
+            if (holdLT == 100) {
+                wheelServo.setPower(0.5);
+                toggleState = 3;
+            } else if (holdRT == 100) {
+                wheelServo.setPower(-0.5);
+                toggleState = 1;
+            } else if (toggleWheelServo) {
+                toggleState = (toggleState + 1) % 4;
+                switch (toggleState) {
+                    case 0:
+                    case 2:
+                        wheelServo.setPower(0);
+                        break;
+                    case 1:
+                        wheelServo.setPower(-0.5);
+                        break;
+                    case 3:
+                        wheelServo.setPower(0.5);
+                        break;
+                }
+            }
+        }
+    }
+}
