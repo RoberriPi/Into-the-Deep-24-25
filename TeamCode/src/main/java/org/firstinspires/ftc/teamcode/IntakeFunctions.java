45cce9a4dfe0d59bc65b8e7828f5519630f30176
Wilke000 <carterwilke@gmail.com>
2024-10-12 11:02:26 -0400

    Fixed Gradle problems

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeFunctions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeFunctions.java
@@ -1,5 +1,7 @@
-package org.firstinspires.ftc.teamcode;
+// Testing Github Desktop
 
+package org.firstinspires.ftc.teamcode;
+// testing github desktop again
 import static java.lang.Thread.sleep;
 
 import android.util.Pair;
@@ -22,10 +24,10 @@ public class IntakeFunctions {
 
     public void intakeWheelServo(boolean toggleWheelServo, float holdLT, float holdRT) {
         if (toggleWheelServo) {
-            if (holdLT == 100) {
+            if (holdLT == 1) {
                 wheelServo.setPower(0.5);
                 toggleState = 3;
-            } else if (holdRT == 100) {
+            } else if (holdRT == 1) {
                 wheelServo.setPower(-0.5);
                 toggleState = 1;
             } else if (toggleWheelServo) {
